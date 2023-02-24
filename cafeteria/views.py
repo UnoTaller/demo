85d436f50dbb82cd0b69e9c080cf154cbfcf98d4
carlosmca44 <carlosmca4424@gmail.com>
2023-02-24 17:38:46 -0500

    basic menu section

--- cafeteria/views.py
+++ cafeteria/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    return render(request,'home/home.html')
+
+
+def menu(request):
+    return render(request, 'menu/menu.html')
