b01133063eb2f5e51d88dd715588778980ce62e4
yandivd <yandivd@gmail.com>
2023-02-24 15:02:06 -0500

    models redes y negocio

--- cafeteria/admin.py
+++ cafeteria/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Especialidad
+from .models import *
 
 # Register your models here.
 admin.site.register(Especialidad)
+admin.site.register(RedSocial)
+admin.site.register(Negocio)
--- cafeteria/models.py
+++ cafeteria/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class RedSocial(models.Model):
+    ico = models.ImageField(upload_to='redes_sociales')
+    nombre = models.CharField(max_length=50)
+    link = models.CharField(max_length=300)
+    def __str__(self):
+        return self.nombre
+class Negocio(models.Model):
+    logo = models.ImageField(upload_to='negocio')
+    nombre = models.CharField(max_length=50)
+    redes = models.ManyToManyField(RedSocial)
+    def __str__(self):
+        return self.nombre
+
 class Especialidad(models.Model):
     nombre = models.CharField(max_length=100)
     receta = models.CharField(max_length=300)
