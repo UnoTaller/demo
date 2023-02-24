8d93082d134047b55345a17aa822b17bf8b54507
yandivd <yandivd@gmail.com>
2023-02-24 14:38:46 -0500

    modelo especialidad creado

--- cafeteria/models.py
+++ cafeteria/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Especialidad(models.Model):
+    nombre = models.CharField(max_length=100)
+    receta = models.CharField(max_length=300)
+    img = models.ImageField(upload_to='especialidades')
+
+    def __str__(self):
+        return self.nombre
